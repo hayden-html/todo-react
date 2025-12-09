bb2fe25121a01c244a8a91bbfb3966ea0e53a79d
Hayden <hayden.110@outlook.com>
2025-12-09 11:48:05 +0000

    using state function to get notes from storage instead of trigger

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,23 @@ interface Note {
 }
 
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [notesTrigger, setNotesTrigger] = useState(0);
-
-  useEffect(() => {
+  const [notes, setNotes] = useState(() => {
     const notesStorage = localStorage.getItem("notes");
-    const notesObject = notesStorage ? JSON.parse(notesStorage) : [];
-    setNotes(notesObject);
-  }, [notesTrigger]);
+    return notesStorage ? JSON.parse(notesStorage) : [];
+  });
+
+  // useEffect(() => {
+  //   const notesStorage = localStorage.getItem("notes");
+  //   const notesObject = notesStorage ? JSON.parse(notesStorage) : [];
+  //   setNotes(notesObject);
+  // }, [notesTrigger]);
 
   return (
     <>
       <div className="p-4">
         <header className="text-xl font-bold mb-4">To Do List</header>
         <main>
-          <AddNoteForm setNotesTrigger={setNotesTrigger} />
+          <AddNoteForm setNotes={setNotes} />
           <div className="">
             <ul>
               {notes?.map((note, index) => (
